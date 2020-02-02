3a267fed311be85a03b312d6f1ea85398942c0aa
Kevin Wells <darkoppressor@gmail.com>
2020-02-01 23:13:42 -0500

    Adjust sizes
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- game_constants.cpp
+++ game_constants.cpp
@@ -14,9 +14,9 @@ using namespace std;
 int32_t Game_Constants::CHUNK_SIZE=0;
 double Game_Constants::TILE_SIZE=0.0;
 int32_t Game_Constants::LOCAL_WORLD_SIZE=0;
+int32_t Game_Constants::CHUNK_CACHE_RANGE=0;
 uint32_t Game_Constants::MAXIMUM_CHUNKS_IN_CACHE=0;
 uint32_t Game_Constants::MAXIMUM_CHUNKS_IN_POOL=0;
-int32_t Game_Constants::CHUNK_CACHE_RANGE=0;
 double Game_Constants::SHIP_COLLISION_REDUCTION=0.0;
 int32_t Game_Constants::SHIP_COLLISION_STEPS=0;
 /// END SCRIPT-GENERATED CONSTANT INITIALIZATIONS
@@ -40,14 +40,14 @@ void Game_Constants_Loader::set_game_constant (string name, string value) {
     else if(name=="local_world_size"){
         Game_Constants::LOCAL_WORLD_SIZE=Strings::string_to_long(value);
     }
+    else if(name=="chunk_cache_range"){
+        Game_Constants::CHUNK_CACHE_RANGE=Strings::string_to_long(value);
+    }
     else if(name=="maximum_chunks_in_cache"){
         Game_Constants::MAXIMUM_CHUNKS_IN_CACHE=Strings::string_to_unsigned_long(value);
     }
     else if(name=="maximum_chunks_in_pool"){
         Game_Constants::MAXIMUM_CHUNKS_IN_POOL=Strings::string_to_unsigned_long(value);
-    }
-    else if(name=="chunk_cache_range"){
-        Game_Constants::CHUNK_CACHE_RANGE=Strings::string_to_long(value);
     }
     else if(name=="ship_collision_reduction"){
         Game_Constants::SHIP_COLLISION_REDUCTION=Strings::string_to_double(value);
